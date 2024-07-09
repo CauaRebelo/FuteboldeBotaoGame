af41203b7e0915cdecc77c3c6d0993d9009272a2
Cauã Rebelo <cauarebelo@discente.ufg.br>
2024-07-09 20:04:11 -0300

    Bolas responsivas

--- JogoDoBotao/Assets/Scripts/ExampleClass.cs
+++ JogoDoBotao/Assets/Scripts/ExampleClass.cs
@@ -5,26 +5,46 @@ public class ExampleClass : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
 
+    public float power = 10;
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
     public Renderer rend;
+    Vector2 force;
+    Vector3 mousePos;
+    Vector3 startPoint;
+    Vector3 endPoint;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
     }
 
+    void OnMouseDown()
+    {
+        // If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Drag started!");
+        mousePos = Input.mousePosition;
+        startPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        startPoint.z = 15;
+    }
     void OnMouseDrag()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = Camera.main.nearClipPlane;
-        Debug.Log(Camera.main.ScreenToWorldPoint(mousePos));
-        rb.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
-        rend.material.color -= Color.white * Time.deltaTime;
+        mousePos = Input.mousePosition;
+        //mousePos.z = Camera.main.nearClipPlane;
+        //Debug.Log(Camera.main.ScreenToWorldPoint(mousePos));
+        //rb.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        //rend.material.color -= Color.white * Time.deltaTime;
     }
 
     void OnMouseUp()
     {
         // If your mouse hovers over the GameObject with the script attached, output this message
         Debug.Log("Drag ended!");
-        rb.transform.position = new Vector3(rb.transform.position.x, rb.transform.position.y, 0);
+        mousePos = Input.mousePosition;
+        endPoint = Camera.main.ScreenToWorldPoint(mousePos);
+        endPoint.z = 15;
+        force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+        rb.AddForce(force * power, ForceMode2D.Impulse);
     }
 }
